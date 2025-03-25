e2130bc59fa2fc105594ccbdc9e8d580e584a549
abene <abenezertileye2003@gmail.com>
2025-03-25 12:18:20 +0300

    event api

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,10 +1,24 @@
 import express from "express";
-import { createBlog, getAllBlogs } from "../controllers/adminController.js";
+import {
+  createBlog,
+  getAllBlogs,
+  createEvent,
+  getAllEvents,
+  getEventById,
+  updateEvent,
+  deleteEvent,
+} from "../controllers/adminController.js";
 
 const router = express.Router();
 
-// Protected Routes (Require Authentication)
-router.post("/blog", createBlog);
-router.get("/blogs", getAllBlogs);
+// Blog Routes
+router.post("/create-blog", createBlog);
+router.get("/get-all-blogs", getAllBlogs);
+// Event Routes
+router.post("/create-event", createEvent);
+router.get("/get-all-events", getAllEvents);
+router.get("/get-event/:id", getEventById);
+router.put("/update-event/:id", updateEvent);
+router.delete("/delete-event/:id", deleteEvent);
 
 export default router;
