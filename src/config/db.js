242e99f1227702aad79a9905db7ecfd14efabb3d
abene <abenezertileye2003@gmail.com>
2025-03-25 11:47:57 +0300

    additional setup

--- src/config/db.js
+++ src/config/db.js
@@ -1,16 +1,11 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.error(`Error: ${error.message}`);
-    process.exit(1);
-  }
+  const conn = mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((err) => console.error("MongoDB connection error:", err));
+  console.log(`MongoDB Connected`);
 };
 
 export default connectDB;
